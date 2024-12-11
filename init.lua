77af2b71cde2d2cb2879d071eb7c5bb92cba8554
Timothy Craig <tcraig@frameshift.io>
2024-12-11 17:40:31 -0500

    Improves telescope searching; plugin updates

--- init.lua
+++ init.lua
@@ -502,7 +502,13 @@ require('lazy').setup({
         -- },
         pickers = {
           find_files = {
-            hidden = true,
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+          },
+          grep_string = {
+            additional_args = { '--hidden' },
+          },
+          live_grep = {
+            additional_args = { '--hidden' },
           },
         },
         extensions = {
@@ -521,8 +527,16 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      -- vim.keymap.set('n', '<leader>sp', function()
+      --   builtin.find_files {
+      --     cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy'),
+      --   }
+      -- end, { desc = '[S]earch [P]ackages' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('v', '<leader>sw', function()
+        builtin.live_grep { default_text = vim.fn.getreg '0' }
+      end, { desc = '[S]earch current highlighted [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
