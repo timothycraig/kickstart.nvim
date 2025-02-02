ee100e96f932c86dd2f3e4a753d18884c2d23ab9
Timothy Craig <tcraig@frameshift.io>
2025-02-01 22:03:02 -0500

    Reenables vue lsp format

--- init.lua
+++ init.lua
@@ -875,7 +875,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, vue = true }
+        local disable_filetypes = { c = true, cpp = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -898,7 +898,6 @@ require('lazy').setup({
         json = { 'prettier' },
         yaml = { 'prettier' },
         -- markdown = { 'prettier' },
-        -- vue = { 'prettier' },
 
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
